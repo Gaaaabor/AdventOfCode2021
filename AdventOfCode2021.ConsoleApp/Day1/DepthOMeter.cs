3a6c4846ef9fb327e9297f82a6b519b5f21c90c9
bocsg <bocsg@DESKTOP-7B3F315>
2021-12-03 22:49:53 +0100

    Add project files.

---
+++ AdventOfCode2021.ConsoleApp/Day1/DepthOMeter.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+
+namespace AdventOfCode2021.ConsoleApp.Day1
+{
+    public class DepthOMeter
+    {
+        public static int HowDepthIncreases(int[] measurements)
+        {
+            var changes = 0;
+
+            for (var i = 1; i < measurements.Length - 1; i++)
+            {
+                if (measurements[i] < measurements[(i + 1)])
+                {
+                    changes++;
+                }
+            }
+
+            return changes;
+        }
+
+        public static int HowDepthIncreases2(int[] measurements)
+        {
+            var changes = 0;
+            var sum = 0;
+
+            for (var i = 1; i < measurements.Length - 2; i++)
+            {
+                var first = measurements[i];
+                var second = measurements[i + 1];
+                var third = measurements[i + 2];
+
+                var temp = first + second + third;
+                if (sum != 0 && sum < temp)
+                {
+                    changes++;
+                }
+
+                sum = temp;
+            }
+
+            return changes;
+        }
+    }
+}
---
+++ AdventOfCode2021.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode2021.ConsoleApp.Day1;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var measurements = File
+                .ReadAllLines("Day1\\Measurements.txt")
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            var i = DepthOMeter.HowDepthIncreases2(measurements);
+            Console.WriteLine($"Total: {i}");
+        }
+    }
+}
